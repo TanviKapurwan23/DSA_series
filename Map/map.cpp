8533dbe05ae36321448017a0ad2e892ff6c74325
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2024-06-10 10:58:54 +0530

    Add files via upload

--- Map/map.cpp
+++ Map/map.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
 
+
+// Map is an associative array
+int main(){
+    map<string, int> marksMap;
+    marksMap["Harray"] = 98;
+    marksMap["Jack"] = 59;
+    marksMap["Rohan"] = 2;
+
+    map<string, int> :: iterator iter;
+    for(iter=marksMap.begin(); iter != marksMap.end(); iter++){
+        cout<<(*iter).first<<" "<<(*iter).second<<"\n";
+    }
+
+    cout<<"the size of the map is "<<marksMap.size()<<endl;
+    cout<<"The max size of the map is "<<marksMap.max_size()<<endl;
+    cout<<"The empty's return value is "<<marksMap.empty()<<endl;
+
+
+    return 0;
+}
